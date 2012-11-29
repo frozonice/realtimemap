29f441b03ffacf71bcefafa6a42e08c81a110054
Martyn <martynrdavies@gmail.com>
2012-11-29 20:34:00 +0100

    adding package

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -14,7 +12,6 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -22,8 +19,16 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  // Displays our index page
+
+});
+
+app.post('/receiver', function(req, res){
+  // Receives events from the webhook on SendGrid
+  // grabs the address and pushes it to the map
+});
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("S-Express is proper dancing on port " + app.get('port'));
 });
