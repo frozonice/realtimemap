eb2bc2d3029fbb6daa9a253f162f069f41bf75fc
Martyn <martynrdavies@gmail.com>
2012-11-29 19:31:31 +0100

    routes gone

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
@@ -28,7 +23,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
