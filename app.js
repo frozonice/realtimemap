4a8c66435f8f8ccdbcd8dd0d9064c86e61f341e9
Martyn <martynrdavies@gmail.com>
2013-04-20 11:30:49 +0200

    change city

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ var express = require('express'),
     md5 = require('MD5'),
     SendGrid = require('sendgrid').SendGrid,
     count = 0,
-    target = 4,
+    target = 25,
     isAcceptingMessages = true;
 
 var pusher = new Pusher({
-  appId: process.env["PUSHER_APP_ID"],
-  key: process.env["PUSHER_APP_KEY"],
-  secret: process.env["PUSHER_APP_SECRET"]
+  appId: process.env['PUSHER_APP_ID'],
+  key: process.env['PUSHER_APP_KEY'],
+  secret: process.env['PUSHER_APP_SECRET']
 });
 
 var channel = 'map';
 
 // Define the SendGrid stuff
-var sendgrid = new SendGrid(process.env["SENDGRID_USERNAME"], process.env["SENDGRID_PASSWORD"]);
+var sendgrid = new SendGrid(process.env['SENDGRID_USERNAME'], process.env['SENDGRID_PASSWORD']);
 
 // Define the app stuff
 var app = express();
@@ -44,7 +44,7 @@ function emailWinner(emailAddress, name){
 
   sendgrid.send({
     to: emailAddress,
-    from: 'martyn.davies@sendgrid.com',
+    from: 'martyn@sendgrid.com',
     subject: 'Boom! You are the winner!',
     text: 'Hey '+name+', you were the 4th email received! Congratulations, you win! See Martyn and he will give you your prize.'
   }, function(success, message) {
@@ -56,12 +56,12 @@ function emailWinner(emailAddress, name){
 
 // Geocoder function
 function geocodeCity(cityName, callback) {
-  var map_url = "http://maps.googleapis.com/maps/api/geocode/json?address="+cityName+"&sensor=false";
+  var map_url = 'http://maps.googleapis.com/maps/api/geocode/json?address='+cityName+'&sensor=false';
   var whenReqComplete = function(error, response, data) {
     if (!error && response.statusCode == 200) {
       var lat = data.results[0].geometry.location.lat;
       var lng = data.results[0].geometry.location.lng;
-      var combinedLatLon = lat+","+lng;
+      var combinedLatLon = lat+','+lng;
     }
     callback(lat, lng, combinedLatLon);
   };
@@ -73,9 +73,9 @@ function geocodeCity(cityName, callback) {
 // Get the index (takes a query string /?city=Amsterdam)
 app.get('/', function(req, res){
   var initialCity = req.query.city;
-  if (!initialCity || initialCity === null){ initialCity = "Austin"; }
+  if (!initialCity || initialCity === null){ initialCity = 'Paris'; }
   geocodeCity(initialCity, function(lat, lng, combinedLatLon){
-    res.render('index', {title: "SendGrid Parse API Demo", latlon: combinedLatLon});
+    res.render('index', {title: 'SendGrid Parse API Demo', latlon: combinedLatLon});
   });
 });
 
@@ -89,8 +89,8 @@ app.post('/receiver', function(req, res){
   }
   // Hash the email address
   var email_address = req.body.from.replace(/</g,'').replace(/>/g,'').split(' ');
-  var sender_name = email_address[0]+" "+email_address[1];
-  console.log("Email address: "+email_address[2]);
+  var sender_name = email_address[0]+' '+email_address[1];
+  console.log('Email address: '+email_address[2]);
   var hashed_email_address = md5(email_address[2]);
 
   geocodeCity(req.body.subject, function(lat, lng, combinedLatLon){
@@ -109,7 +109,7 @@ app.post('/receiver', function(req, res){
       if (err) {
         console.log(err);
       } else {
-        console.log("Sending "+ data.email +" at location "+ data.lat+","+data.lng);
+        console.log('Sending '+ data.email +' at location '+ data.lat+','+data.lng);
       }
     });
 
@@ -122,5 +122,5 @@ app.post('/receiver', function(req, res){
 });
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("S-Express is proper dancing on port " + app.get('port'));
+  console.log('S-Express is proper dancing on port ' + app.get('port'));
 });
